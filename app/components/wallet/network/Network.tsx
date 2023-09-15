b4db2a2d565e23e17d3ae9f5677dde8391d0c6d3
Welliton Gervickas <wellitogervickas@gmail.com>
2023-09-15 17:19:03 -0300

    fix: wallet panel network colors

--- app/components/wallet/network/Network.tsx
+++ app/components/wallet/network/Network.tsx
@@ -52,19 +52,29 @@ export const Network = ({ chain, chains, ...props }: NetworkProps) => {
             <li
               className={classNames(
                 'cursor-pointer bg-black/70 p-2 hover:bg-black/40 hover:text-white',
-                ' flex space-x-2'
+                'flex items-center justify-between space-x-2'
               )}
               onClick={() => switchNetwork && switchNetwork(chainItem.id)}
             >
               <span>{chainItem.name}</span>
-              <span>
-                {chain.id === chainItem.id && (
-                  <CheckBadgeIcon width={18} className='text-green-400' />
+              <span className='flex items-center space-x-px'>
+                <span>
+                  {chain.id === chainItem.id && (
+                    <CheckBadgeIcon
+                      width={18}
+                      className='text-green-400'
+                      title='conectado'
+                    />
+                  )}
+                </span>
+                {chainItem.testnet === true && (
+                  <PuzzlePieceIcon
+                    width={18}
+                    className='text-dark-green'
+                    title='testnet'
+                  />
                 )}
               </span>
-              {chainItem.testnet === true && (
-                <PuzzlePieceIcon width={18} className='text-red-400' />
-              )}
             </li>
           ))
         )}
