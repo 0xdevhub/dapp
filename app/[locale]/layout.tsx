1ff0dfdd4726d6def9cf1a086c544b4210a01a5f
Welliton Gervickas <wellitogervickas@gmail.com>
2023-09-22 16:37:55 -0300

    fix: provider

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,27 +19,29 @@ export default function BaseLayout({ children }: BaseLayoutProps) {
 
   return (
     <WalletProvider>
-      <div className={isClient ? (isDarkMode ? 'dark' : 'light') : 'dark'}>
-        <div
-          className={classNames(
-            'flex min-h-screen flex-col',
-            'h-screen overflow-x-hidden text-sm font-normal',
-            'bg-gradient-radial',
-            'from-stone-100 via-stone-200 to-stone-300 text-stone-600/70',
-            'dark:from-third-100 dark:via-third-300 dark:to-third-500 dark:text-gray-400'
-          )}
-        >
-          {process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true' ? (
-            <MaintenanceMode />
-          ) : (
-            <LanguageProvider>
-              <Header />
-              <main className='flex-1 container'>{children}</main>
-              <Footer className='py-12 text-black/30' />
-            </LanguageProvider>
-          )}
+      <LanguageProvider>
+        <div className={isClient ? (isDarkMode ? 'dark' : 'light') : 'dark'}>
+          <div
+            className={classNames(
+              'flex min-h-screen flex-col',
+              'h-screen overflow-x-hidden text-sm font-normal',
+              'bg-gradient-radial',
+              'from-stone-100 via-stone-200 to-stone-300 text-stone-600/70',
+              'dark:from-third-100 dark:via-third-300 dark:to-third-500 dark:text-gray-400'
+            )}
+          >
+            {process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true' ? (
+              <MaintenanceMode />
+            ) : (
+              <>
+                <Header />
+                <main className='flex-1 container'>{children}</main>
+                <Footer className='py-12 text-black/30' />
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      </LanguageProvider>
     </WalletProvider>
   )
 }
