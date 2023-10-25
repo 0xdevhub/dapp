b1dec839987ce13c6c12f8c02c9dea54fccc3b72
Welliton Gervickas <wellitogervickas@gmail.com>
2023-10-24 22:27:35 -0300

    chore: partial redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/en/',
+        destination: '/en/apps/',
+        permanent: false
+      },
+      {
+        source: '/pt/',
+        destination: '/pt/apps/',
+        permanent: false
+      }
+    ]
+  },
   transpilePackages: ['next-international', 'international-types']
 }
 
