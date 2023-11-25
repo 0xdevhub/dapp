655ffe32c14f40569d74a09ab8623dabb997f97a
welliton gervickas <wellitogervickas@gmail.com>
2023-11-25 20:00:49 -0300

    fix: adding contracts

--- app/config/network.ts
+++ app/config/network.ts
@@ -2,9 +2,19 @@ import { Chain, avalancheFuji } from 'wagmi/chains'
 import { merge, reduce } from 'lodash'
 import { getChainRPCById } from '../lib/utils/network'
 
-export const AVALANCHE = merge(avalancheFuji, {
+export const AVALANCHE: Chain = merge(avalancheFuji, {
   rpcUrls: {
     protocol: getChainRPCById(avalancheFuji.id)
+  },
+  contracts: {
+    accessManagement: {
+      address: '0x1987F5A465c27dD03d25a029d66a3eD800228EC9',
+      blockCreated: 0
+    },
+    hub: {
+      address: '0x53E13Dd84Dc418095c31655573504F9A588F9159',
+      blockCreated: 0
+    }
   }
 })
 
